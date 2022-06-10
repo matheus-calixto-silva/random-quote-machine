be3b113f51d3fa5869c63bc555ec1f96674997a3
Matheus Calixto <matheuscalixto8@gmail.com>
2022-06-10 19:13:49 -0300

    feat: calling api with usestate and useeffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,39 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { useEffect, useState } from 'react';
+import './App.scss';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    callApi();
+  }, []);
+
+  function handleClick() {
+    setData(null);
+    callApi();
+  }
+
+  async function callApi() {
+    const quoteJson = await (await fetch('https://favqs.com/api/qotd')).json();
+    setData(quoteJson);
+  }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div id='quote-box'>
+      <div id='quote'>
+        <h1 id='text'>“{data && data.quote.body}”</h1>
+        <p id='author'>{data && data.quote.author}</p>
+      </div>
+      <div id='buttons'>
+        <a href='twitter.com/intent/tweet' target='_blank' id='tweet-quote'>
+          Tweet
+        </a>
+        <button id='new-quote' onClick={handleClick}>
+          New Quote
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
